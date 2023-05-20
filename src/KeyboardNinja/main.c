d934cd8aa04f41fa6aad14365b320bfa6989926c
Denis Bykov <1337228den@bk.ru>
2023-05-20 15:12:53 +0700

    Add src folder and splitted main.c file

---
+++ src/KeyboardNinja/main.c
@@ -0,0 +1,54 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <libKeyboardNinja/funcs.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define BG_RED 64
+#define BG_GREEN 32
+#define FG_GRAY 2
+#else
+#include <unistd.h>
+#define BG_RED 41
+#define FG_GRAY 30
+#define BG_GREEN 42
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    printf("\t\t —— TYPING TRAINER —— \n\n");
+
+    char filename[100] = "test.txt";
+    char text[1000];
+    char user_input[1000];
+    struct timeval start, end;
+
+    read_file(filename, text, sizeof(text)); // Читаем текст из файла
+    print_text(text); // Выводим текст на экран
+
+    gettimeofday(&start, NULL); // Начинаем отсчет времени
+    read_user_input(
+            user_input,
+            sizeof(user_input)); // Читаем введенный пользователем текст
+    gettimeofday(&end, NULL); // Заканчиваем отсчет времени
+
+    double elapsed = end.tv_sec - start.tv_sec
+            + (end.tv_usec - start.tv_usec)
+                    / 1000000.0; // Вычисляем прошедшее время в секундах
+
+    int errors = print_comparison(
+            text, user_input); // Сравниваем введенный текст с оригинальным и
+                               // считаем количество ошибок
+    print_results(
+            errors,
+            strlen(text),
+            elapsed); // Выводим результаты: количество ошибок, процент ошибок и
+                      // время набора
+
+    return 0;
+}
