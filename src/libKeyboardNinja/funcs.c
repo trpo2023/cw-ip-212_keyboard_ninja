c0f15a8ecf8dc9cf266bf2abce3ce38c2eab53cf
Vadim Glinskiy <v.glinskiy@icloud.com>
2023-05-20 16:33:39 +0700

    apply clang-format to funcs.c

--- src/libKeyboardNinja/funcs.c
+++ src/libKeyboardNinja/funcs.c
@@ -1,10 +1,10 @@
 #include "funcs.h"
 #include <ctype.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <locale.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -20,17 +20,18 @@
 #define BG_GREEN 42
 #endif
 
-// Функция для удаления лишних символов 
-void trim(char *str) {
+// Функция для удаления лишних символов
+void trim(char* str)
+{
     // Удаление с правого края
     int len = strlen(str);
-    while (len > 0 && isspace(str[len-1])) {
-        str[len-1] = '\0';
+    while (len > 0 && isspace(str[len - 1])) {
+        str[len - 1] = '\0';
         len--;
     }
-    
+
     // Удаление с левого края
-    char *start = str;
+    char* start = str;
     while (*start && isspace(*start)) {
         start++;
     }
@@ -38,7 +39,8 @@ void trim(char *str) {
 }
 
 // Меняем цвет текста
-void set_text_color(int color) {
+void set_text_color(int color)
+{
 #ifdef _WIN32
     // для ОС Windows используется библиотека Windows.h,
     // получаем дескриптор стандартного вывода и устанавливаем цвет текста
@@ -52,7 +54,8 @@ void set_text_color(int color) {
 }
 
 // Функция для вывода оригинального текста
-void print_text(const char* text) {
+void print_text(const char* text)
+{
     printf("\nType the following text: %s\n", text);
 
     set_text_color(FG_GRAY);
@@ -61,8 +64,9 @@ void print_text(const char* text) {
 }
 
 // Функция для считывания текста из файла
-void read_file(const char* filename, char* buffer, size_t buffer_size) {
-    FILE *file = fopen(filename, "r");
+void read_file(const char* filename, char* buffer, size_t buffer_size)
+{
+    FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
         exit(1);
@@ -73,7 +77,8 @@ void read_file(const char* filename, char* buffer, size_t buffer_size) {
 }
 
 // Функция для получения ввода пользователя
-void read_user_input(char* buffer, size_t buffer_size) {
+void read_user_input(char* buffer, size_t buffer_size)
+{
     printf("\n");
     fgets(buffer, buffer_size, stdin);
 
@@ -81,7 +86,8 @@ void read_user_input(char* buffer, size_t buffer_size) {
 }
 
 // Выводим сравнение двух строк, а также считаем и возвращаем кол-во ошибок
-int print_comparison(const char* original_text, const char* user_input) {
+int print_comparison(const char* original_text, const char* user_input)
+{
     printf("\nResult text:\n");
     int errors = 0;
     int i;
@@ -89,8 +95,7 @@ int print_comparison(const char* original_text, const char* user_input) {
     int user_len = strlen(user_input);
 
     for (i = 0; original_text[i] != '\0'; i++) {
-        if (i >= user_len)
-        {
+        if (i >= user_len) {
             set_text_color(FG_GRAY);
             errors++;
             putchar(original_text[i]);
@@ -114,10 +119,11 @@ int print_comparison(const char* original_text, const char* user_input) {
 }
 
 // Выводим всю информацию
-void print_results(int errors, int total_chars, double elapsed_time) {
-    double error_percentage = (double) errors / total_chars * 100;
+void print_results(int errors, int total_chars, double elapsed_time)
+{
+    double error_percentage = (double)errors / total_chars * 100;
     printf("\n\nErrors: %d\n", errors);
-    printf("Accuracy percentage: %.2f%%\n", 100.0-error_percentage);
+    printf("Accuracy percentage: %.2f%%\n", 100.0 - error_percentage);
     printf("Elapsed time: %.2f seconds\n\n", elapsed_time);
 }
 
